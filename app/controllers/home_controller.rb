3e385b080d38236c1fcccfa92e4401482564ba1c
Kim SungSoo <ss4483@naver.com>
2016-05-07 03:25:50 +0000

    수정

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,29 +10,17 @@ class HomeController < ApplicationController
      new_post.content = params[:content]
      new_post.img_file = params[:img_file]
      new_post.save
-<<<<<<< HEAD
      
      redirect_to "/read_h/#{new_post.id}"
-=======
-    
-     redirect_to "/list_h"
->>>>>>> 05af6c4f8511a28d2e572680b235d7629e5947d8
   end
   def read_h
     @one_post = Post.find(params[:post_id])
   end
-<<<<<<< HEAD
 
   def destroy_h
     one_post = Post.find(params[:post_id])
     one_post.destroy
     redirect_to :back
-=======
-  def destroy
-     @one_post = Post.find(params[:post_id])
-     @one_post.destroy
-     redirect_to "/list_h"
->>>>>>> 05af6c4f8511a28d2e572680b235d7629e5947d8
   end
   def update_view_h
      @one_post = Post.find(params[:post_id]) 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index_h'
-<<<<<<< HEAD
   get '/write_view_h' => "home#write_view_h"
   post '/write_h' => "home#write_h"
   
@@ -9,15 +8,6 @@ Rails.application.routes.draw do
   get '/destroy_h/:post_id' => "home#destroy_h"
   get '/update_view_h/:post_id' => "home#update_view_h"
   post 'update_h/:post_id' => "home#update_h"
-=======
-  get '/write_h' => "home#write_h"
-  get '/read_h' => "home#read_h"
-  post '/read_h' => "home#read_h"
-  get '/list_h' => "home#list_h"
-  get 'destroy/:post_id'=>"home#destroy"
-  get 'update_view_h/:post_id' => "home#update_view_h"
-  post 'update/:post_id' => "home#update"
->>>>>>> 05af6c4f8511a28d2e572680b235d7629e5947d8
   get '/' => "home#index_h"
   post '/' => "home#index_h"
   get '/home/index_s'
