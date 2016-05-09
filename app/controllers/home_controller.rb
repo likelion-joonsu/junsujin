01766d351df7ff756424ed7bd9a857045cb7bad9
ssujjin <j_jin0713@ynu.ac.kr>
2016-05-09 12:16:45 +0000

    test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -38,4 +38,45 @@ class HomeController < ApplicationController
      @one_post.save
      redirect_to "/read_h/#{@one_post.id}"
   end
+  
+  def index_s
+  end
+  def write_view_s
+    
+    new_post = Writing.new
+     new_post.subject = params[:subject]
+     new_post.stuff = params[:stuff]
+     new_post.img_file = params[:img_file]
+     new_post.save
+     
+  end
+  def write_s
+     new_post = Writing.new
+     new_post.subject = params[:subject]
+     new_post.stuff = params[:stuff]
+     new_post.img_file = params[:img_file]
+     new_post.save
+     
+     redirect_to "/read_h/#{new_post.id}"
+  end
+  def read_s
+    @one_writing = Writing.find(params[:post_num])
+  end
+
+  def destroy_s
+    one_post = Writing.find(params[:post_num])
+    one_post.destroy
+    one_post.save   
+    redirect_to "/home/index_s"
+  end
+  def update_view_s
+     @one_post = Post.find(params[:post_num]) 
+  end
+  def update_s
+     @one_post = Post.find(params[:post_num])
+     @one_post.title = params[:title]
+     @one_post.content = params[:content]
+     @one_post.save
+     redirect_to "/read_s/#{@one_post.id}"
+  end
 end
